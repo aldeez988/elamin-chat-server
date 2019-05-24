04af277a4d3d83658d012b26794166dd5391d72c
Glitch (hello-express) <none>
2019-05-24 15:24:47 +0000

    🖐🍡 Checkpoint ./server.js:4409242/44

--- server.js
+++ server.js
@@ -68,8 +68,7 @@ const {id,from ,text}=request.body;
   // if(!id && !from || !text ){
   //   response.status(400).json('Please enter complete data for update')
   // }
-  const messageCopy =messages.slice();
-messages = messageCopy.map(message=>{
+messages = messages.map(message=>{
   if(message.id ===id)
     {
       if(from!==""){
