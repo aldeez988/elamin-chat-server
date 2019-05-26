a06f8d419eb500bdf3dd8151633ddcd27587abca
Glitch (hello-express) <none>
2019-05-26 12:33:40 +0000

    🐧🛎 Checkpoint ./server.js:4409242/9

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 const welcomeMessage = {
   id: 0,
